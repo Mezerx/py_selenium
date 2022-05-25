e726ce9a25f1fe7ea61c259c20af076d37363fd4
Yauhen Lis <jfox1389@yandex.ru>
2022-05-25 20:17:46 +0300

    25/05

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome()
-link = "http://suninjuly.github.io/cats.html"
-browser.get(link)
-browser.find_element_by_id("button")
+browser.get("http://suninjuly.github.io/registration2.html")
+browser.find_element_by_tag_name("input").send_keys("Ivan")
+browser.find_element_by_name("last_name").send_keys("Petrov")
+browser.find_element_by_class_name("city").send_keys("Smolensk")
+browser.find_element_by_id("country").send_keys("Russia")
+browser.find_element_by_xpath('//*[@type="submit"]').click()
+
+
+
 
 
 
